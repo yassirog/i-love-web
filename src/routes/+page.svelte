ab9c14da9a9ca8d8f51292f851dfc879f41be88d
yassir bghiel <yassir.bghiel@hva.nl>
2024-12-07 01:34:47 +0100

    Startup v2
    Opnieuw startup files met een begin aan de homepagina, zonder duplicate files.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,108 @@
+<div class="wrapper">
+	<div class="container"></div>
+
+	<div class="components">
+		<h1>Learning Journal</h1>
+		<div class="text-block">
+			<p>
+				Hallo en welkom op mijn (Yassir's) persoonlijke Learning Journal! Dit is de plek waar ik
+				mijn leerproces, projecten en groei als Frontender documenteer. Hier deel ik mijn successen,
+				uitdagingen en alles wat ik onderweg ontdek. Van nieuwe concepten en technieken tot
+				creatieve projecten en reflecties op mijn voortgang, je vindt het allemaal hier. Mijn doel
+				is niet alleen om mijn eigen ontwikkeling bij te houden, maar ook om te inspireren en kennis
+				te delen met anderen die dezelfde reis maken.
+			</p>
+		</div>
+		<a href="kiespagina" class="btn-default"> Ga naar de kiespagina </a>
+	</div>
+</div>
+
+<style>
+	:root {
+		color-scheme: dark;
+
+		--green: 0, 255, 0;
+		--white: 255, 255, 255;
+
+		--primary-rgb: var(--green);
+		--primary: rgb(var(--primary-rgb));
+		--background: rgb(4, 4, 4);
+		--background-light: rgba(var(--primary-rgb), 0.1);
+		--background-hover: rgba(var(--primary-rgb), 0.4);
+		--border: rgba(var(--primary-rgb), 0.2);
+		--border-radius: 6px;
+		--blur: blur(4px);
+	}
+
+	html,
+	body {
+		background-color: var(--background);
+		margin: 0;
+		box-sizing: border-box;
+	}
+
+	.wrapper {
+		font-family: Consolas, monospace, sans-serif;
+	}
+
+	.container {
+		height: 100%;
+	}
+
+	p {
+		color: var(--primary);
+		margin: 0;
+	}
+
+	.components {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+	}
+
+	h1 {
+		margin-top: 3em;
+		color: var(--primary);
+		font-size: 32px;
+	}
+
+	.text-block {
+		margin-top: 5em;
+		max-width: 32ch;
+	}
+
+	.text-block:hover {
+		border-color: var(--border);
+	}
+
+	.text-block p {
+		margin: 0;
+		text-align: justify;
+		color: var(--primary);
+	}
+
+	.btn-default {
+		margin: 20px;
+		padding: 10px 20px;
+		backdrop-filter: var(--blur);
+		background-color: var(--background-light);
+		border: 2px solid var(--border);
+		border-radius: var(--border-radius);
+		color: var(--primary);
+		font-size: 18px;
+		font-family: Consolas, monospace, sans-serif;
+		font-weight: bold;
+		cursor: pointer;
+		transition:
+			background-color 0.4s,
+			border-color 0.4s,
+			backdrop-filter 0.4s;
+	}
+
+	.btn-default:hover {
+		background-color: var(--background-hover);
+		border-color: var(--primary);
+	}
+</style>
---
+++ src/routes/kiespagina/+page.svelte
@@ -0,0 +1,3 @@
+hoi 
+hoi
+hopi
